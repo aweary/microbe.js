e4540c5f791b517867f9697b583245e6cb2f0b30
Brandon Dail <Kierkegaurd@gmail.com>
2015-05-25 15:56:40 -0500

    Added additional comments Updated example view "Help" using Handlebars Fixed responsify omission

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+/* Example implementation of Microbe.js server and router */
+
 var server	= require('./microbe/server.js');
 var Router	= require('./microbe/router.js');
-var fs			= require('fs');
 
 var router = new Router('/');
 var server = server(router, 3000);
@@ -10,5 +11,5 @@ router.get('/', function(request, response) {
 })
 
 router.get('/help', function(request, response) {
-  response.render('help');
+  response.render('help', {title: 'Help!'});
 })
--- microbe/server.js
+++ microbe/server.js
@@ -1,44 +1,20 @@
-#! /home/vagrant/.nvm/versions/node/v0.12.0/bin/node --harmony
+var http				= require('http');
+var url					= require('url');
+var responsify	= require('./response');
 
-const 
+module.exports = function(router, port) {
 
-	http 		= require('http'),
-	url  		= require('url'),
-	responsify  = require('./response');
+  var port = typeof port === 'number' ? port : parseInt(port);
+  if (isNaN(port)) throw new Error('Port must be a numerical value');
 
+  var server = http.createServer(function(request, response) {
 
-module.exports = function(router, port){
+    var requestType = request.method.toString().toLowerCase();
+    responsify(request, response);
+    router._handleRequest(requestType, request, response);
 
+  });
 
-	var port = typeof port === 'number' ? port : parseInt(port);
-	if(isNaN(port)) throw new Error('Port must be a numerical value');
-
-
-	var server = http.createServer(function(request, response){
-
-		var requestType = request.method.toString().toLowerCase();
-		responsify(request, response);
-		router._handleRequest(requestType, request, response);
-
-	});
-
-	server.listen(port, function(){ console.log('Listening on port ' + port) });
-
+  server.listen(port, function() { console.log('Listening on port ' + port) });
 
 }
-
-
-
-
-/*
-
-
-var router = new Router(server);
-
-router.get('/', function(req, res){
-	
-
-})
-
-
- */
