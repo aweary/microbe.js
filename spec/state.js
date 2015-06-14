bca24d258839f29237feab090eff065095e8d79b
Brandon Dail <Kierkegaurd@gmail.com>
2015-06-14 15:18:15 -0500

    Updated unit tests

--- spec/state.js
+++ spec/state.js
@@ -13,34 +13,34 @@ describe('State', function() {
   it('should update the view location with app.set', function() {
 
     app.set('views', 'test');
-    expect(app._state.views).to.equal('test');
+    expect(app.state.views).to.equal('test');
 
   });
 
   it('should return "views" as the defualt view location', function() {
 
-    expect(app._state.views).to.equal('views');
+    expect(app.state.views).to.equal('views');
   });
 
   it('should return an empty array for middleware by default', function() {
-    expect(app._state.middleware).to.be.a('array');
-    expect(app._state.middleware.length).to.equal(0);
+    expect(app.state.middleware).to.be.a('array');
+    expect(app.state.middleware.length).to.equal(0);
   });
 
   it('should return an empty array for routes', function() {
-    expect(app._state.routes).to.be.a('array');
-    expect(app._state.routes.length).to.equal(0);
+    expect(app.state.routes).to.be.a('array');
+    expect(app.state.routes.length).to.equal(0);
   });
 
   it('should add declared routes to state.routes', function() {
 
     app.route('/', {});
-    expect(app._state.routes).to.contain('/');
+    expect(app.state.routes).to.contain('/');
   });
 
   it('should have an empty static routes', function(){
-    expect(app._state.staticRoutes).to.be.a('array');
-    expect(app._state.staticRoutes.length).to.equal(0);
+    expect(app.state.staticRoutes).to.be.a('array');
+    expect(app.state.staticRoutes.length).to.equal(0);
   })
 
 
