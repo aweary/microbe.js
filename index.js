af51007d1ead2d01d79020759b27b46980f0eb19
Brandon Dail <Kierkegaurd@gmail.com>
2015-09-14 22:52:27 -0500

    Fixed route handler casing issue for method

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Application from './lib/prototype'
 
-export default function microbe(opts) {
-  return new Application(opts)
+export default function microbe(root) {
+  return new Application(root)
 }
